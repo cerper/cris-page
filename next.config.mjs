d6435308ef544bc6794fd0a018db6cdd4624f6ed
root <root@DESKTOP-ATFCIID>
2024-06-16 20:41:30 -0400

    (prueba) llamada api prueba 4

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    esmExternals: "loose", // <-- add this
+    serverComponentsExternalPackages: ["mongoose"], // <-- and this
+  },
+  // and the following to enable top-level await support for Webpack
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
