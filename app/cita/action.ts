a17606c29e9e3aa171d2929db15f36d04ef20f2a
root <root@DESKTOP-ATFCIID>
2024-06-18 10:37:54 -0400

    (agregando)script

--- app/cita/action.ts
+++ app/cita/action.ts
@@ -4,6 +4,7 @@ import { prisma } from "@/lib/db/prisma";
 import { redirect } from "next/navigation";
 
 export async function addUser(form: FormData) {
+  console.log(form);
   const Nombre = form.get("Nombre")?.toString();
   const Apellido = form.get("Apellido")?.toString();
   const Telefono = form.get("Telefono")?.toString();
